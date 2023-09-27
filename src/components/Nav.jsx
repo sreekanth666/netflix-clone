9eafbcab979bd42f2da238b5d33a8550026f4b5a
Sreekanth K <srekthk@gmail.com>
2023-09-27 11:34:31 +0530

    UI Improvements

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,20 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './Nav.css'
+import { useEffect } from 'react'
 
 function Nav() {
+    const [show, setShow] = useState(false)
+    useEffect(()=> {
+        window.addEventListener("scroll", ()=>{
+            if(window.scrollY>600){
+                setShow(true)
+            } else {
+                setShow(false)
+            }
+        })
+    })
     return (
-        <div className='nav'>
+        <div className={`nav ${show && 'navBlack'}`}>
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="Logo" style={{
                 width: '150px'
             }} />
